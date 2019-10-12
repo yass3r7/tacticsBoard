57e4157f6ef1c134eb72883dd31d7999b46b43e0
yasser <yasser007boubani@gmail.com>
2019-10-12 15:59:44 +0100

    finis all menues and ui animation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
 $(function () {
     // controls hide/show on double click on the board
-    $("#board").dblclick(function () {
-        $(".controls").fadeToggle();
+    $("#board").dblclick(function (e) {
+        if (e.target == board) {
+            $(".item-options").hide();
+            $(".controls").fadeToggle();
+        }
     });
 
     // options menu
     $(".menu").click(function () {
         $(".options").slideToggle();
         $(".add-item-menu").hide();
+        $(".item-options").slideUp("fast");
     })
 
     // option addItem()
@@ -18,23 +22,57 @@ $(function () {
 
     // add a specific item
     $(".add-item-menu ul li").click(function () {
-        console.log($(this).data('item'));
-
+        // console.log($(this).data('item'));
         $(".add-settings").show("slow");
     });
-    
+
+    // component settings
+    $(".component").dblclick(function () {
+        item = $(this).data("name");
+        item_id = $(this).attr("id");
+        
+        if ($(this).data("type") == "vec") {
+            $(".controls").fadeIn();
+            $(".item-options").slideUp("fast");
+            $(".item-options").slideDown();
+        } else if ($(this).data("type") == "img") {
+            console.log("image");
+        }
+    });
+
+    $("#itemProp").click(function () {
+        $(".item-options").hide();
+        $(".item-settings").show("slow");
+        $(".vec-item-settings").removeClass("hidden");
+    });
 });
+
+// confirm adding item
+function confirmAdding() {
+    let num = howMany.value;
+
+    if (num < 1 || num > 100) {
+        alert("PLease, choose a number between 1 and 100");
+        return false;
+    }
+
+    $(".add-settings").hide("slow");
+    $(".add-item-menu").slideUp();
+}
 
 // cancel adding item
 function cancelAdding() {
     $(".add-settings").hide("slow");
 }
 
-// confirm adding item
-function confirmAdding() {
-    let num = howMany.value;
+// confirm changes
+function confirmChanges() {
+    $(".item-settings").hide("slow");
+    $(".vec-item-settings").addClass("hidden");
+}
 
-    $(".add-settings").hide("slow");
-    $(".add-item-menu").slideUp();
-    console.log(num);
+// cancel changes
+function cancelChanges() {
+    $(".item-settings").hide("slow");
+    $(".vec-item-settings").addClass("hidden");
 }
