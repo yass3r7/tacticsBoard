1eb28c4860a33035c07107d455599c718ffedefa
yasser <yasser007boubani@gmail.com>
2019-10-12 10:45:23 +0100

    finished options menu

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(function () {
-    
+    // controls hide/show on double click on the board
+    $("#board").dblclick(function () {
+        $(".controls").fadeToggle();
+    });
+
+    // options menu
+    $(".menu").click(function () {
+        $(".options").slideToggle();
+    })
+
+    // option addItem()
+    // function addItem() {
+
+    // }
 });
